f8622d670498acc01b946266afa1f4fdd1862ff8
Hemanthkumargj <hemanth.kumar@skizzle.tech>
2025-10-17 23:48:45 +0530

    Oct 14 changes Added

--- Task/Task005.py
+++ Task/Task005.py
@@ -6,7 +6,9 @@ print("a is greater than b" if a>b else "a is less than b")
 
 
 number = float(input("Enter the Number:"))
-if number > 100:
+if number >= 100:
     print("number is greater than 100")
 else:
     print("number is less than 100")
+
+print("Number is Grater that 100" if number >=100 else "Number is less than 100")
